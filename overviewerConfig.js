479741533184c7cfb2d88e271204ed619d8f5f46
kukamaka <55360255+kukamaka@users.noreply.github.com>
2022-04-08 23:41:02 +0300

    jklj

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1648955772",
+        "cacheTag": "1649449427",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world - overworld",
-            "last_rendertime": 1648952534,
+            "last_rendertime": 1649442160,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
